2a26116d81b1cd4aa420c6ba49224b8aa858f492
Xiaodown <xiaodown@gmail.com>
2025-08-03 20:41:44 -0700

    adjusting the way the numbers look, convert to F

--- src/sense_reader.py
+++ src/sense_reader.py
@@ -1,14 +1,12 @@
 def get_sensor_data():
     from sense_hat import SenseHat
-    import time
 
     sense = SenseHat()
-    
-    # Read temperature and humidity, rounded to 1 decimal place
-    temperature = round(sense.get_temperature(), 1)
+    temperature_c = sense.get_temperature()
+    temperature_f = round((temperature_c * 9/5) + 32, 1)
     humidity = round(sense.get_humidity(), 1)
     
     return {
-        'temperature': temperature,
+        'temperature': temperature_f,
         'humidity': humidity
     }
